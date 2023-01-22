7a228d406df0e70056264a2f33d03537494d9068
Kseniya <89151102337@mail.ru>
2023-01-22 19:04:28 +0300

    Seminar_4 ConsoleApp_4

--- Seminars/Seminar_4/ConsoleApp_4/Program.cs
+++ Seminars/Seminar_4/ConsoleApp_4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int [] FillRandomArray (int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr [i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] array = FillRandomArray(8);
+PrintArray(array);
