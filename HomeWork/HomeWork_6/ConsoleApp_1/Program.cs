745523ae3fe5eb5ffa763081339d0325a826f16b
Kseniya <89151102337@mail.ru>
2023-02-02 17:29:34 +0300

    HomeWork_6 ConsoleApp_1

--- HomeWork/HomeWork_6/ConsoleApp_1/Program.cs
+++ HomeWork/HomeWork_6/ConsoleApp_1/Program.cs
@@ -2,15 +2,33 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь. 
 // Ввод чисел останавливается при помощи ввода стоп-слова "stop" и производится при помощи нажатия Enter
 
-int positive = 0;
-while (Console.ReadKey().KeyChar != "stop")
+// int positive = 0;
+// while (Console.ReadKey().KeyChar != "stop")
+// {
+//     Console.WriteLine ("Введите число: ");
+//     int n = Convert.ToInt32(Console.ReadLine());
+//     if (n > 0)
+//     {
+//         positive += 1;
+//     }
+// }
+// Console.WriteLine($"Положительных числ: {positive}");
+
+int number;
+int count = 0;
+string? answer = null;
+string stopWord = "stop";
+
+while (true)
 {
-    Console.WriteLine ("Введите число: ");
-    int n = Convert.ToInt32(Console.ReadLine());
-    if (n > 0)
+    answer = Console.ReadLine();
+    if (answer == stopWord)
     {
-        positive += 1;
+        break;
     }
+    number = Convert.ToInt32(answer);
+    if (number > 0) count++;
 }
-Console.WriteLine($"Положительных числ: {positive}");
+Console.WriteLine($"Положительных числ: {count}");
+
 
