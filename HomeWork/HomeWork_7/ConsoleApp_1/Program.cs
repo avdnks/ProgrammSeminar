9538722a251f941c2f8df2cef7412e53392d5fb1
Kseniya <89151102337@mail.ru>
2023-02-02 23:33:44 +0300

    HomeWork_7

---
+++ HomeWork/HomeWork_7/ConsoleApp_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetRandomMatrix (int rows, int columns, int leftRange, int rightRange)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const int LEFT_RANGE = 0;
+const int RIGHT_RAHNGE = 10;
+
+double[,] array = GetRandomMatrix (ROWS, COLUMNS, LEFT_RANGE, RIGHT_RAHNGE);
+PrintMatrix(array);
---
+++ HomeWork/HomeWork_7/ConsoleApp_3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetRandomMatrix (int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int [rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+// double GetAverageColumns (int[,] matrix)
+// {
+//     double average = 0;
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         double sum = 0;
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+//             sum += matrix[i, j];
+//         }
+//         average = sum / matrix.GetLength(0);
+//         Console.Write($"{average}");
+//     }
+//     return average;
+// }
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const int LEFT_RANGE = 0;
+const int RIGHT_RAHNGE = 10;
+
+int[,] array = GetRandomMatrix (ROWS, COLUMNS, LEFT_RANGE, RIGHT_RAHNGE);
+PrintMatrix(array);
+// int average = GetAverageColumns(array);
+// Console.WriteLine($"Среднее равно: {average}");
+
+double average = 0;
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    average = sum / array.GetLength(0);
+    Console.Write($"{average:f2}" + " ");
+}
