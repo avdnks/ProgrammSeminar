5afbe1b121306e657b97bbaf617dcf67aa7d6868
Kseniya <89151102337@mail.ru>
2023-02-04 23:23:24 +0300

    Seminar_8

---
+++ Seminars/Seminar_8/ConsoleApp_1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет 
+// местами первую и последнюю строку массива.
+
+int[,] GetRandomMatrix (int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int [rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MatrixM (int[,] matrix)
+{
+    int temp = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix [0, j];
+        matrix[0, j] = matrix [matrix.GetLength(0) - 1, j];
+        matrix [matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const int LEFT_RANGE = 0;
+const int RIGHT_RAHNGE = 10;
+
+int[,] array = GetRandomMatrix (ROWS, COLUMNS, LEFT_RANGE, RIGHT_RAHNGE);
+PrintMatrix(array);
+Console.WriteLine();
+MatrixM (array);
+PrintMatrix(array);
+
---
+++ Seminars/Seminar_8/ConsoleApp_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+
+int[,] GetRandomMatrix (int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] matrix = new int [rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+int [,] TMatrix (int[,] matrix)
+{
+    int[,] matrix2 = new int [matrix.GetLength(1), matrix.GetLength(0)];
+    for (int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            matrix2[i, j] = matrix[j, i];
+        }
+    }
+    return matrix2;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+const int ROWS = 3;
+const int COLUMNS = 4;
+const int LEFT_RANGE = 0;
+const int RIGHT_RAHNGE = 10;
+
+int[,] array = GetRandomMatrix (ROWS, COLUMNS, LEFT_RANGE, RIGHT_RAHNGE);
+PrintMatrix(array);
+Console.WriteLine();
+int[,] matrix2 = TMatrix(array);
+PrintMatrix(matrix2);
+
