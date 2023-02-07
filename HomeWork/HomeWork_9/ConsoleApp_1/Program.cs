d5cba8243fc79715155f32295b395068f7813f53
Kseniya <89151102337@mail.ru>
2023-02-07 18:24:18 +0300

    HomeWork_9

---
+++ HomeWork/HomeWork_9/ConsoleApp_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1.
+
+void PrintNaturalNumbersToLow (int n, int count)
+{
+    if (count > n) return;
+    PrintNaturalNumbersToLow (n, count + 1);
+    Console.Write(count + " ");
+}
+
+int number = 5;
+int count = 1;
+PrintNaturalNumbersToLow(number, count);
---
+++ HomeWork/HomeWork_9/ConsoleApp_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumMN (int m, int n)
+{
+    int sum = 0;
+    if (m == n) return n;
+    sum += m + SumMN(m + 1, n);
+    return sum;
+}
+
+const int M = 1;
+const int N = 15;
+Console.Write(SumMN(M, N));
